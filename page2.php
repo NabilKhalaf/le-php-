5e725d0b71569533e0751ae62cd5f0abab231ef8
Nabil Khalaf <nabil.khalaf.pro@gmail.com>
2016-11-24 10:11:05 +0100

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,32 @@
+<html>
+  <header>
+      <h1> Exercices avec PHP sur les boucles </h1>
+  </header>
+
+<h2> Question 2 <h2>
+
+<h3> 1 <h3>
+
+<?php
+  for ($i=100; $i>0 ;$i--) {
+    echo ($i);
+    echo ("-");
+  }
+ ?>
+
+ <hr/>
+
+<h3> 2 <h3>
+
+<?php
+  $i=101;
+  while($i > 1) {
+    $i--;
+    echo $i;
+    echo "-";
+
+  }
+
+?>
+
+</html>
