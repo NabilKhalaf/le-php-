3933d4c8420228cf520b023e2f37466d303720f9
Nabil Khalaf <nabil.khalaf.pro@gmail.com>
2016-11-24 10:10:27 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+  <header>
+      <h1> Exercices avec PHP sur les boucles </h1>
+  </header>
+
+<h2> Question 1 <h2>
+
+<h3> 1 <h3>
+
+  <?php
+  for ($i=1; $i<101;$i++) {
+    echo ($i);
+    echo ("-");
+  }
+ ?>
+
+ <hr/>
+
+<h3> 2 <h3>
+
+  <?php
+  $i=1;
+  while($i < 101) {
+    echo $i;
+    echo "-";
+    $i++;
+  }
+
+  ?>
+
+</html>
